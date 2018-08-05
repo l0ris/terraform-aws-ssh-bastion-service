634de74f477e758fca6ba46db088b1edbd24070c
Joshua Kite <joshuamkite@gmail.com>
2018-08-05 20:30:19 +0100

    update ami query interpolation

--- main.tf
+++ main.tf
@@ -176,7 +176,7 @@ data "aws_ami" "ubuntu" {
 
   filter {
     name   = "name"
-    values = ["ubuntu-bionic-18.04-amd64-server-*"]
+    values = ["ubuntu/images/hvm-ssd/ubuntu-bionic-18.04-amd64-server-*"]
   }
 
   filter {
